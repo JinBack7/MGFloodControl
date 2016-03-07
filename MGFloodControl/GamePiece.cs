aca1291e2657b8faba1d84ce6caac4e46dc9e1d9
JinBack7 <jinyang123@live.at>
2016-03-07 22:41:42 +0100

    Completed the GamePiece and started doing GameBoard

--- MGFloodControl/GamePiece.cs
+++ MGFloodControl/GamePiece.cs
@@ -135,6 +135,42 @@ namespace MGFloodControl
                     break;
             }
         }
+
+        public string[] GetOtherEnds(string startingEnd)
+        {
+            List<string> opposites = new List<string>();
+
+            foreach (string end in pieceType.Split(','))
+            {
+                if (end != startingEnd) 
+                {
+                    opposites.Add(end);
+                }
+            }
+
+            return opposites.ToArray();
+        }
+
+        public bool HasConnector(string direction)
+        {
+            return pieceType.Contains(direction);
+        }
+
+        public Rectangle GetSourceRect() 
+        {
+            int x = textureOffsetX;
+            int y = textureOffsetY;
+
+            if (pieceSuffix.Contains("W")) 
+            {
+                x = +PieceWidth + texturePaddingX;
+            }
+
+            y += (Array.IndexOf(PieceTypes, pieceType) * (PieceHeight + texturePaddingY));
+
+            return new Rectangle(x, y, PieceWidth, PieceHeight);
+        }
+
     }
 
     
